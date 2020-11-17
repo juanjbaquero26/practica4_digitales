907c9829a7533bd1a04309897a5e59f83b054133
jehosua <jehosua.narvaez@udea.edu.co>
2020-11-17 18:23:46 -0500

    Update circuito.vhd

--- practia4_digitales/practia4_digitales.srcs/sources_1/new/circuito.vhd
+++ practia4_digitales/practia4_digitales.srcs/sources_1/new/circuito.vhd
@@ -18,30 +18,37 @@
 -- 
 ----------------------------------------------------------------------------------
 
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
-entity circuito is
-Port ( pix_x : in STD_LOGIC_VECTOR (9 downto 0);
+entity draw is
+    Port ( pix_x : in STD_LOGIC_VECTOR (9 downto 0);
            pix_y : in STD_LOGIC_VECTOR (9 downto 0);
-           sw : in STD_LOGIC_VECTOR (1 downto 0);
+           sw : in STD_LOGIC_VECTOR (2 downto 0);
            video_on: in std_logic;
            rgb_out : out STD_LOGIC_VECTOR (11 downto 0));
-end circuito;
+end draw;
 
-architecture Behavioral of circuito is
+architecture Behavioral of draw is
+
+signal negro: STD_LOGIC_VECTOR (11 downto 0)=x"000";
+signal rojo: STD_LOGIC_VECTOR (11 downto 0)= x"F00";
+signal verde: STD_LOGIC_VECTOR (11 downto 0)=x"0B4";
+signal azul: STD_LOGIC_VECTOR (11 downto 0)=x"0F0";
+signal blanco: STD_LOGIC_VECTOR (11 downto 0)=x"FFF";
+signal negro: STD_LOGIC_VECTOR (11 downto 0)=x"4B0";
 
 begin
 
+process (sw)
+	begin
+	case sw is
+		when "000" => if (((pix_x >= 140) and (pix_x <= 500) and (pix_y >= 90) and (pix_y <= 190))) then rgb_out <= blanco; 
+					 elsif (((pix_x >= 140) and (pix_x <= 500) and (pix_y >= 190) and (pix_y <= 290))) then rgb_out <= azul;
+					 elsif (((pix_x >= 140) and (pix_x <= 500) and (pix_y >= 290) and (pix_y <= 390))) then rgb_out <= rojo;
+					 else rgb_out <= negro;
 
 end Behavioral;
+
+
